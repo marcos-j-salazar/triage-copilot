7e9339f55dc9cc5ec1696194526315086db91c4a
Marcos <salazarjmarcos@gmail.com>
2026-09-06 01:09:57 -0400

    Fixed source field to use a default instead of being required

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ templates = Jinja2Templates(directory=".")
 class UpdateDataRequest(BaseModel):
     text: str = Field(..., min_length=1)
     correct_category: str = Field(..., min_length=1)
-    source: str = Field(..., min_length=1, description="Source  of the correction")
+    source: str = Field(default="staff_corrected", min_length=1, description="Source of the correction")
 
 class PredictRequest(BaseModel):
     text: str = Field(..., min_length=1, description="Student's request, typed by Staff")
